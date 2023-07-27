418a155994f1cc3b7b59a96a8e948b40619d6332
Juan Fajardo <juanfar.group@gmail.com>
2023-07-27 20:37:54 +0200

    feat: Initial prisma configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FixtureModule } from './features/fixture/fixture.module';
 import { TournamentModule } from './features/tournament/tournament.module';
 
 @Module({
   imports: [FixtureModule, TournamentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
